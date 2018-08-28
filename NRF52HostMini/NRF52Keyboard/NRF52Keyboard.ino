2ded08a0f8ccfcb4188cc412ccfd035ce3ca7dc7
Mike Wu <mail@wumike.com>
2018-08-28 07:24:49 +0700

    Setup NRF52 Bluetooth

--- NRF52HostMini/NRF52Keyboard/NRF52Keyboard.ino
+++ NRF52HostMini/NRF52Keyboard/NRF52Keyboard.ino
@@ -1,18 +1,21 @@
-
-
 #include <Arduino.h>
 #include <SoftwareSerial.h>
+#include <bluefruit.h>
 
 char hidReport[23];
 byte inByte;
 int byteIndex;
 bool skipSeg = false;
 
-#define SW_RXD    A0
-#define SW_TXD    A1
+/*
+ * Software Serial for Debugging
+ * BLE doesn't work with Software Serial.
+ */
+//#define SW_RXD    A0
+//#define SW_TXD    A1
 
 // Declare an Software Serial instance
-SoftwareSerial mySerial(SW_RXD, SW_TXD);
+//SoftwareSerial mySerial(SW_RXD, SW_TXD);
 
 // Keyboard Keycodes
 uint8_t modifier;
@@ -23,24 +26,68 @@ uint8_t key3;
 uint8_t key4;
 uint8_t key5;
 
+BLEDis bledis;
+BLEHidAdafruit blehid;
+
 void setup() {
 
-  while (!Serial);
-  delay(500);
+  // Serial.begin(115200);
+  Serial.begin(9600);   // Baud rate for USB Host
+  
+  //Serial.println("Ready to rock..");
 
-  Serial.begin(115200);
-  Serial.println("Ready to rock..");
+  //mySerial.begin(9600);
+  //Serial.println("UART ready ready");
 
-  mySerial.begin(9600);
-  Serial.println("UART ready ready");
+  Bluefruit.begin();
+  Bluefruit.setTxPower(4);
+  Bluefruit.setName("Kinesis Advantage 2");
+
+  bledis.setManufacturer("WU Tech");
+  bledis.setModel("K2ADV1");
+  bledis.begin();
+
+  blehid.begin();
+
+  //blehid.setKeyboardLedCallback(set_keyboard_led);
+  
+  startAdv();
+}
+
+void startAdv(void)
+{  
+  // Advertising packet
+  Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
+  Bluefruit.Advertising.addTxPower();
+  Bluefruit.Advertising.addAppearance(BLE_APPEARANCE_HID_KEYBOARD);
+  
+  // Include BLE HID service
+  Bluefruit.Advertising.addService(blehid);
+
+  // There is enough room for the dev name in the advertising packet
+  Bluefruit.Advertising.addName();
+  
+  /* Start Advertising
+   * - Enable auto advertising if disconnected
+   * - Interval:  fast mode = 20 ms, slow mode = 152.5 ms
+   * - Timeout for fast mode is 30 seconds
+   * - Start(timeout) with timeout = 0 will advertise forever (until connected)
+   * 
+   * For recommended advertising interval
+   * https://developer.apple.com/library/content/qa/qa1931/_index.html   
+   */
+  Bluefruit.Advertising.restartOnDisconnect(true);
+  Bluefruit.Advertising.setInterval(32, 244);    // in unit of 0.625 ms
+  Bluefruit.Advertising.setFastTimeout(30);      // number of seconds in fast mode
+  Bluefruit.Advertising.start(0);                // 0 = Don't stop advertising after n seconds
 }
 
 void loop() {
 
-  while (mySerial.available()) {
+  while (Serial.available()) {
 
     // Read every byte to remove from buffer
-    inByte = mySerial.read();
+    inByte = Serial.read();
 
     // New key report, read segment
     if ((int) inByte == 10) {
@@ -63,13 +110,15 @@ void loop() {
 
         assignKeyCodes(hidReport);
 
-        Serial.write(modifier);
-        Serial.write(key0);
-        Serial.write(key1);
-        Serial.write(key2);
-        Serial.write(key3);
-        Serial.write(key4);
-        Serial.write(key5);
+//        Serial.write(modifier);
+//        Serial.write(key0);
+//        Serial.write(key1);
+//        Serial.write(key2);
+//        Serial.write(key3);
+//        Serial.write(key4);
+//        Serial.write(key5);
+
+        blehid.keyboardReport(modifier,key0,key1,key2,key3,key4,key5);
 
 
         byteIndex = 0;
@@ -80,6 +129,8 @@ void loop() {
     }
     
   }
+
+  waitForEvent();  
 
 }
 
